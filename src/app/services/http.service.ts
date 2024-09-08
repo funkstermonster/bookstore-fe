a723313eac8fcf38be5e81184d854d7f8457dcb3
funkstermonster <fanni.szasz96@gmail.com>
2024-09-08 16:53:18 +0200

    create http service

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  private apiUrl: string = 'http://localhost:8000/api/books';
+
+  constructor(private http: HttpClient) {}
+
+  public getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  public getBook(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
+  public addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  public updateBook(id: number, book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+  }
+
+  public deleteBook(id: number) {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
