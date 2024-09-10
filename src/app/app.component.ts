f7adfdf9bb3ea8a8611e8993c0a6460f6c2ee490
funkstermonster <fanni.szasz96@gmail.com>
2024-09-10 15:57:14 +0200

    modify app component, create new interface, modify styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HttpService } from './services/http.service';
 import { Observable } from 'rxjs';
 import { Book } from './models/book';
@@ -10,16 +10,11 @@ import { Book } from './models/book';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent, RouterModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  books$: Observable<Book[]> = new Observable<Book[]>;
-  constructor(private httpService: HttpService) {}
-
-  ngOnInit(): void {
-    this.books$ = this.httpService.getBooks();
-  }
+  
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, Routes } from '@angular/router';
+import { BooksOverviewComponent } from './components/books-overview/books-overview.component';
+import { HomeComponent } from './components/home/home.component';
 
+export const router: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "books", component: BooksOverviewComponent },
+  { path: "**", redirectTo: "home", pathMatch: 'full' }
+];
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideHttpClient()]
+  providers: [provideRouter(router), provideAnimations(), provideHttpClient(), ],
 };
