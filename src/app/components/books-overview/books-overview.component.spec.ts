3b43c22e8320da98f06fc5fe7df2e070f91ba469
funkstermonster <fanni.szasz96@gmail.com>
2024-09-12 15:38:55 +0200

    create tests for books table component

--- src/app/components/books-overview/books-overview.component.spec.ts
+++ src/app/components/books-overview/books-overview.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BooksOverviewComponent } from './books-overview.component';
+import { CommonModule } from '@angular/common';
+import { BooksTableComponent } from '../books-table/books-table.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BooksOverviewComponent', () => {
   let component: BooksOverviewComponent;
@@ -8,7 +13,14 @@ describe('BooksOverviewComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [BooksOverviewComponent]
+      imports: [
+        BooksOverviewComponent,
+        CommonModule,
+        BooksTableComponent,
+        MatDialogModule,
+        MatButtonModule,
+        RouterTestingModule,
+      ],
     });
     fixture = TestBed.createComponent(BooksOverviewComponent);
     component = fixture.componentInstance;
--- src/app/components/books-table/books-table.component.spec.ts
+++ src/app/components/books-table/books-table.component.spec.ts
@@ -1,21 +1,81 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BooksTableComponent } from './books-table.component';
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableResponsiveDirective } from 'src/app/directives/mat-table-responsive.directive';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogConfig } from '@angular/material/dialog';
 
 describe('BooksTableComponent', () => {
   let component: BooksTableComponent;
   let fixture: ComponentFixture<BooksTableComponent>;
+  let dialog: jasmine.SpyObj<MatDialog>;
 
   beforeEach(() => {
+    const dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+
     TestBed.configureTestingModule({
-      imports: [BooksTableComponent]
-    });
+      imports: [
+        BooksTableComponent,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableResponsiveDirective,
+        MatButtonModule,
+        MatIconModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [{ provide: MatDialog, useValue: dialogSpy }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(BooksTableComponent);
     component = fixture.componentInstance;
+    dialog = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open confirmation dialog on delete', () => {
+    const mockData = { id: 1, name: 'Test Book' };
+
+    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
+    dialogRefSpy.afterClosed.and.returnValue(of(true));
+
+    dialog.open.and.returnValue(dialogRefSpy);
+
+    // Ensure deleteDialogConfig includes only title and message
+    component.deleteDialogConfig = {
+      data: {
+        title: 'Confirm Delete',
+        message: 'Are you sure?',
+      },
+    } as MatDialogConfig;
+
+    component.onDelete(mockData);
+
+    expect(dialog.open).toHaveBeenCalledWith(
+      ConfirmationDialogComponent,
+      jasmine.objectContaining({
+        data: jasmine.objectContaining({
+          title: 'Confirm Delete',
+          message: 'Are you sure?',
+        }),
+      })
+    );
+
+    component.delete.subscribe((deletedItem) => {
+      expect(deletedItem).toEqual(mockData);
+    });
   });
 });
